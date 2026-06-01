6fbbb2fc68c74c7a393aedd1038bba88a0e681fa
Istiaque-Mahbub <istiaquemahbu1@gmail.com>
2026-06-01 19:21:14 +0600

    remove unused createSpecialityZodSchema validation schema

--- src/zod/auth.validation.ts
+++ src/zod/auth.validation.ts
@@ -29,6 +29,3 @@ export const loginValidationZodSchema = z.object({
     }),
 });
 
-export const createSpecialityZodSchema = z.object({
-    title:z.string().min(3, "Title must be at least 3 characters long")
-})
